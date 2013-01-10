432357e09adc0188f3768497ff99016342d8c67d
Alexandre Prates <ajfprates@gmail.com>
2013-01-10 12:25:33 -0200

    Iniciando implementacao do mapkit

---
+++ ContatosNoMapaViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContatosNoMapaViewController.h
+//  ContatosIP67
+//
+//  Created by ios3105 on 10/01/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContatosNoMapaViewController : UIViewController
+
+@end
